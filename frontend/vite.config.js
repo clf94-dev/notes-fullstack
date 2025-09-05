75c8c5e1186509be47e7ec59a82f6eba04dcaf84
clf94-dev <clf94@hotmail.es>
2025-09-05 08:42:46 +0200

    config tests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,24 @@
 /* eslint-disable no-undef */
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-  ],
+  plugins: [react(), svgr()],
   resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, '/src') }],
+    alias: [{ find: "@", replacement: resolve(__dirname, "/src") }],
   },
-
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/tests/setupTests.js",
+    css: true,
+    globals: true,
+    coverage: {
+      reporter: ["text", "lcov"],
+      include: ["src/**/*.{js,jsx}"],
+      exclude: ["src/test/**"],
+    },
+  },
 });
